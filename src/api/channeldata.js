63aea2de354e5f16d803fde7451d8bb9343f9b52
Cerek Hillen <crockeo@gmail.com>
2015-07-24 00:15:30 -0400

    Did some really weird callback voodoo to let the API consumer decide if it wanted users, messages, both, or neither.

--- src/api/channeldata.js
+++ src/api/channeldata.js
@@ -17,10 +17,6 @@ var sockethelper = require('../sockets/helper.js'),
 // Getting the current set of users for a given channel (and calling the
 // callback) with theie value.
 function getUsers(req, callback) {
-    if (req.query.channel === undefined)
-        callback(new Error('A channel name is required to query for users.'), []);
-
-    console.log(typeof req.query.channel);
     sockethelper.getChannelUsers(req.query.channel, function (users) {
         callback(null, users);
     });
@@ -39,9 +35,6 @@ function constructMessageQuery(req, callback) {
 
 // Getting the set of messages from a given server request.
 function getMessages(req, callback) {
-    if (req.query.channel === undefined)
-        callback(new Error('A channel name is required to query for messages.'), []);
-
     constructMessageQuery(req, function (err, query) {
         if (err)
             return callback(err, []);
@@ -57,35 +50,70 @@ function getMessages(req, callback) {
 
 // Handling a GET request on this API endpoint.
 function get(req, res) {
-    getUsers(req, function (err, users) {
+    if (req.query.channel === undefined) {
+        return res.json({
+            error  : true,
+            success: false,
+            message: 'You must provide a channel name to this API endpoint.'
+        });
+    }
+
+    var fns = [],
+        n   = 0;
+
+    // Getting the users if necessary.
+    if (req.query.users === undefined || req.query.users === 'true') {
+        console.log(':)');
+        fns.push(function (data, callback) {
+            getUsers(req, function (err, users) {
+                if (err)
+                    callback(err, data);
+
+                data.users = users;
+
+                callback(null, data);
+            });
+        });
+    }
+
+    // Getting the messages if necessary.
+    if (req.query.messages === undefined || req.query.messages === 'true') {
+        fns.push(function (data, callback) {
+            getMessages(req, function (err, messages) {
+                if (err)
+                    callback(err, data);
+
+                data.messages = messages;
+
+                callback(null, data);
+            });
+        });
+    }
+
+    // Adding the function to publish channel info.
+    fns.push(function (data, callback) {
+        res.json({
+            error  : false,
+            success: JSON.stringify(data) !== '{}',
+            message: JSON.stringify(data) === '{}' ? 'You didn\'t request any data.' : 'Successfully retrieved channel data.',
+            data   : data
+        });
+    });
+
+    // Iterating through the functions in the
+    var caller = function (err, data) {
         if (err) {
             return res.json({
                 error  : err,
                 success: false,
-                message: 'Failed to get users.'
+                message: 'Failed to construct channel data response: ' + String(err)
             });
         }
 
-        getMessages(req, function (err, messages) {
-            if (err) {
-                return res.json({
-                    error  : err,
-                    success: false,
-                    message: 'Failed to get messages.'
-                });
-            }
+        fns[n++](data, caller);
+    };
 
-            res.json({
-                error  : false,
-                success: true,
-                message: 'Successfully got users & channel info.',
-                data   : {
-                    users   : users,
-                    messages: messages
-                }
-            });
-        });
-    });
+    caller(null, {});
 }
 
 /////////////
